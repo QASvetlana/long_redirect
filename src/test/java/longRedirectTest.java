fd15fe4195a46d49a8d8a071d7e9893677c9067f
Sveta Andreeva <89111378147v@yandex.com>
2022-10-30 06:59:21 +0300

    added test

---
+++ src/test/java/longRedirectTest.java
@@ -0,0 +1,42 @@
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+
+public class longRedirectTest {
+    @Test
+    public void testRestAssured() {
+
+        Response response = given()
+                .redirects()
+                .follow(false)
+                .when()
+                .get("https://playground.learnqa.ru/api/long_redirect")
+                .andReturn();
+
+        int statusCode = response.getStatusCode();
+        String name = response.getHeader("Location");
+        System.out.println(statusCode);
+        System.out.println(name);
+
+
+        if (statusCode != 200) {
+            Response locations = RestAssured
+                    .given()
+                    .redirects()
+                    .follow(false)
+                    .when()
+                    .get(name)
+                    .andReturn();
+            String location1 = locations.getHeader("Location");
+            System.out.println(location1);
+        } else {
+
+            System.out.println(200);
+        }
+    }
+}
+
+
+
